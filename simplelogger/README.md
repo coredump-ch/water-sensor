3848ca2702d3a7a1547b64eb2ad3e4d3b452e171
Danilo Bargen <mail@dbrgn.ch>
2016-05-23 00:40:22 +0200

    Rewrite simplelogger in node/typescript

--- simplelogger/README.md
+++ simplelogger/README.md
@@ -1,34 +1,12 @@
-A very simple logger for particle measurement events.
+# Simple SSE logger
 
-Current status: Broken due to https://bitbucket.org/btubbs/sseclient/issues/6/
+Installing:
 
-## Generating a Particle Cloud API token
+    $ npm install
+    $ npm run build
 
-See docs at https://docs.particle.io/reference/api/#create-an-oauth-client and
-https://docs.particle.io/reference/api/#generate-an-access-token.
-Replace all values inside `<angle brackets>` with your own values.
+Running:
 
-Export your credentials:
+    $ node logger.js
 
-    $ export PARTICLE_USERNAME=<joe@example.com>
-    $ export PARTICLE_PASSWORD=<your-password>
-
-Get an access token:
-
-    $ curl https://api.particle.io/oauth/token \
-        -u particle:particle -d grant_type=password \
-        -d username="$PARTICLE_USERNAME" -d password="$PARTICLE_PASSWORD"
-
-Create an OAuth client:
-
-    $ curl https://api.particle.io/v1/clients \
-        -d name=<client-name> -d type=installed \
-        -d access_token=<access-token>
-
-Get a refresh token using your new client:
-
-    $ curl https://api.particle.io/oauth/token \
-        -u <client-id>:<client-secret> -d grant_type=password \
-        -d username="$PARTICLE_USERNAME" -d password="$PARTICLE_PASSWORD"
-
-You should now have a client id, a client secret and a refresh token.
+The logger will log all measurements in CSV format to `log.csv`.
---
+++ simplelogger/logger.ts
@@ -0,0 +1,72 @@
+///<reference path="types/request-promise.d.ts" />
+
+'use strict';
+
+let EventSource = require('eventsource');
+let request = require('request-promise');
+let json = require('json-promise');
+var fs = require('fs');
+
+let PARTICLE_CLIENT_ID = 'changeme'
+let PARTICLE_CLIENT_SECRET = 'changeme'
+let PARTICLE_REFRESH_TOKEN = 'changeme'
+
+
+/**
+ * Return a particle access token.
+ */
+async function getAccessToken(): Promise<string> {
+    // Request options
+    let options = {
+        method: 'POST',
+        uri: 'https://api.particle.io/oauth/token',
+        auth: {
+            'user': PARTICLE_CLIENT_ID,
+            'pass': PARTICLE_CLIENT_SECRET,
+        },
+        form: {
+            'grant_type': 'refresh_token',
+            'refresh_token': PARTICLE_REFRESH_TOKEN,
+        }
+    };
+
+    // Fetch token
+    console.info('Fetching access token...')
+    let body = await request(options).catch((err) => {
+        console.error('Could not retrieve access token (HTTP ' + err.response.statusCode + ')')
+        console.error('Are your refresh token and client credentials correct?')
+        throw err;
+    });
+
+    // Parse response
+    let data = await json.parse(body);
+    return data.access_token;
+}
+
+/**
+ * Process a status change event.
+ */
+function processStatus(event: any) {
+    let data = JSON.parse(event.data);
+    console.info(data.published_at, 'Status update:', data.data);
+}
+
+/**
+ * Process a measurement event.
+ */
+function processMeasurement(event: any) {
+    let data = JSON.parse(event.data);
+    console.info(data.published_at, 'Measurement:', data.data);
+    fs.appendFile('log.csv', data.published_at + ',' + data.data + '\n');
+}
+
+async function main() {
+    let token = await getAccessToken();
+    let url = 'https://api.particle.io/v1/devices/events?access_token=' + token;
+    console.info('Connecting to', url);
+    let es = new EventSource(url);
+    es.addEventListener('spark/status', processStatus);
+    es.addEventListener('measurement', processMeasurement);
+}
+
+main();
