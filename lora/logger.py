f0f0c8e17095a5231c56f967b53434542f918915
Danilo Bargen <mail@dbrgn.ch>
2016-12-10 19:55:52 +0100

    Parse incoming format

--- lora/logger.py
+++ lora/logger.py
@@ -1,10 +1,13 @@
 # pip install paho-mqtt
 import json
+import base64
+import struct
 import paho.mqtt.client as mqtt
 from pprint import pprint
 
 APP_EUI = ''
 ACCESS_KEY = ''
+PAYLOAD_FORMAT = '!iff'
 
 
 def on_connect(client, userdata, flags, rc):
@@ -14,10 +17,24 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, msg):
-    print('%s' % msg.topic)
+    print('\n%s' % msg.topic)
     data = json.loads(msg.payload.decode('utf8'))
     pprint(data)
-    print()
+
+    if msg.topic.endswith('/up'):
+        # Upstream message
+        payload = data.get('payload')
+        if payload is None:
+            print('Upstream msg is missing payload')
+            return
+        bytestring = base64.b64decode(payload)
+        try:
+            unpacked = struct.unpack(PAYLOAD_FORMAT, bytestring)
+        except struct.error as e:
+            print('Invalid payload format: %s' % e)
+            return
+        msg = 'Temp: %.3f°C | Voltage: %.2f | Charge: %.2f%%'
+        print(msg % (unpacked[0] / 1000, unpacked[1], unpacked[2]))
 
 
 client = mqtt.Client()
